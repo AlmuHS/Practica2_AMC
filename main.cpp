dc5e9c461856530dc1ca4e4aaff63150ed666bc8
AlmuHS <liberamenso10000@gmail.com>
2016-11-27 13:18:20 +0000

    add GPLv3 headers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/* Copyright 2016 Almudena García Jurado-Centurión
+    This file is part of Practica2_AMC.
+
+    Practica2_AMC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Practica2_AMC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Practica2_AMC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <iostream>
 #include "include/GenGraph.h"
 #include "include/MinLenghtProblem.h"
@@ -7,7 +25,7 @@ using namespace std;
 
 
 void test_MLP(){
-    GenGraph GenG("d493.tsp");
+    GenGraph GenG("ch130.tsp");
     MinLenghtProblem MLP;
     float minimal_lenght;
     int node_pair[2];
@@ -58,8 +76,8 @@ void test_TSP(){
 int main()
 {
 
-    test_MLP();
-    //test_TSP();
+    //test_MLP();
+    test_TSP();
 
     return 0;
 }
--- src/TSPProblem.cpp
+++ src/TSPProblem.cpp
@@ -1,3 +1,21 @@
+/* Copyright 2016 Almudena García Jurado-Centurión
+    This file is part of Practica2_AMC.
+
+    Practica2_AMC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Practica2_AMC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Practica2_AMC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "../include/TSPProblem.h"
 
 using namespace std;
@@ -19,6 +37,7 @@ void TSPProblem::GenSet(){
         node_set.insert(node_vector[i]);
     }
 }
+
 
 float TSPProblem::SimpleSolution(){
     float minimal, new_min, length;
